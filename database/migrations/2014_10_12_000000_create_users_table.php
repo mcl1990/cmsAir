1e768dc1ce0ab80328953aac1623ac8608be623f
Usuario <jp@intelli-next.com>
2018-05-29 15:45:38 -0400

    comentario

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -28,7 +28,7 @@ class CreateUsersTable extends Migration
                 $table->integer('total_aportes')->default(0);
                 $table->integer('avatar_id')->default(1);
                 $table->rememberToken();
-                $table->timestamps();
+                // $table->timestamps();
             });
             
         }
